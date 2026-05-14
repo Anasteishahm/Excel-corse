ac2fa0311be85d98af032822fe307c10201a43e0
Tomsooer <tsoer2013@gmail.com>
2026-05-14 20:40:32 +0300

    Programming

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,15 +1,31 @@
+import {capitalize} from '@core/utils';
+
 export class DomListener {
   constructor($root, listeners = []) {
-	  if (!$root) {
-		  // eslint-disable-next-line no-tabs
-		  throw new Error('No $root provided for DomListener!');
-	  }
+    if (!$root) {
+      // eslint-disable-next-line no-tabs
+      throw new Error('No $root provided for DomListener!');
+    }
     this.$root = $root;
+    this.listeners = listeners;
   }
 
   initDOMListener() {
-
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        const name = this.name || '';
+        throw new Error(
+            `Method ${method} is not implemented in ${this.name} Component`);
+      }
+      this.$root.on(listener, this[method].bind(this));
+    });
   }
 
-  removeDOMListeners() {}
+  removeDOMListeners() {
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
 }
