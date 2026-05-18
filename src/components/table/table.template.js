72de9b24544c1474d9fc703506017c36cd33b086
Tomsooer <tsoer2013@gmail.com>
2026-05-18 18:41:49 +0300

    Programming

---
+++ src/components/table/table.template.js
@@ -0,0 +1,50 @@
+const CODES = {
+  A: 65,
+  Z: 90,
+};
+
+function createCell() {
+  return `
+<div class="cell" contentEditable>B2</div>
+	`;
+}
+
+function createCol() {
+  return `
+	<div class="column">
+	A
+	</div>
+	`;
+}
+
+function createRow(content) {
+  return `
+	<div class="row">
+	   <div class="row-info"></div>
+	   <div class="row-data">${content}</div>
+	</div>
+	`;
+}
+
+export function createTable(rowsCount = 15) {
+  const colsCount = CODES.Z - CODES.A + 1;
+  const rows = [];
+
+  const cols = new Array(colsCount)
+      .fill('')
+      .map((el, index) => {
+		  // eslint-disable-next-line no-tabs
+		  return String.fromCharCode(CODES.A + index)
+      });
+
+
+  console.log(cols);
+
+  rows.push(createRow());
+
+  for (let i = 0; i < rowsCount; i++) {
+	  rows.push(createRow());
+  }
+
+  return rows.join('');
+}
